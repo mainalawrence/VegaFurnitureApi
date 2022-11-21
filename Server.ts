1d14290fe0acc2b0012ca9900d7e4673818618f1
maina lawrence <mainalawrence32@gmail.com>
2022-11-21 10:42:21 +0300

    api

--- Server.ts
+++ Server.ts
@@ -6,35 +6,48 @@ import ProductsRoute from "./Routes/ProductsRoute";
 import orderRoute from "./Routes/orderRoute";
 import Auth from "./Routes/AuthRoute";
 import connect from "./Database/Connect"
+import compression from 'express'
 
+const path = require('path')
 
 dotenv.config();
 
-const app =express();
+const app = express();
+app.use(compression());
 //access controller
-app.use(cors());
+
 app.use(express.json());
 app.use(express.static('Images'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.get('/static', express.static(path.join(__dirname, 'furnitureApp')));
+
+app.get('/api', function (req, res) {
+  res.sendFile(`/`, { root: path.join(__dirname, 'furnitureApp') });
+});
+
 //connect to database
-``
 try {
-  // dbConnection();
   connect
-} catch (error:any) {
-    console.log("Error :"+error.message);
+} catch (error: any) {
+  console.log("Error :" + error.message);
 }
 
+
 //Main routes
-app.use("/api",UsersRoute);
+app.use("/api", UsersRoute);
 
-app.use("/api",ProductsRoute);
+app.use("/api", ProductsRoute);
 
-app.use("/api",orderRoute);
+app.use("/api", orderRoute);
 
-app.use("/auth",Auth);
+app.use("/auth", Auth);
 
-const port=process.env.PORT||4000;
+const port = process.env.PORT || 4000;
 
-app.listen(port,()=>{
-    console.log(`Server Listening at port ${port}`);
+app.listen(port, () => {
+  console.log(`Server Listening at port ${port}`);
 });
--- build/dist/Server.js
+++ build/dist/Server.js
@@ -5,22 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const cors_1 = __importDefault(require("cors"));
 const UsersRoute_1 = __importDefault(require("./Routes/UsersRoute"));
 const ProductsRoute_1 = __importDefault(require("./Routes/ProductsRoute"));
 const orderRoute_1 = __importDefault(require("./Routes/orderRoute"));
 const AuthRoute_1 = __importDefault(require("./Routes/AuthRoute"));
 const Connect_1 = __importDefault(require("./Database/Connect"));
+const express_2 = __importDefault(require("express"));
+const path = require('path');
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, express_2.default)());
 //access controller
-app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.static('Images'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+app.get('/static', express_1.default.static(path.join(__dirname, 'furnitureApp')));
+app.get('/api', function (req, res) {
+    res.sendFile(`/`, { root: path.join(__dirname, 'furnitureApp') });
+});
 //connect to database
-``;
 try {
-    // dbConnection();
     Connect_1.default;
 }
 catch (error) {
