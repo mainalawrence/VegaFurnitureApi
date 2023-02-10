526e6abcc337f208e40aee894f655b6f35b434d6
maina lawrence <mainalawrence32@gmail.com>
2023-02-10 15:51:00 +0300

    app

--- Database/Connect.ts
+++ Database/Connect.ts
@@ -1,12 +1,10 @@
 import sqlconnection from '../Database/configaration'
 
 try {
-    const res=sqlconnection.connect((res)=>{
-    console.log(`database connected `);
-})
-} catch (error:any) {
-    console.log("Error :"+error.message);
+    const res = sqlconnection.connect((res) => {
+        console.log(`database connected `);
+    })
+} catch (error: any) {
+    console.log("Error :" + error.message);
 }
-
-
 export default sqlconnection;
